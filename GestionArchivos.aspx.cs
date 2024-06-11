22321adfd882283fdfd53d55ffe201f0dc442659
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-06-10 22:09:59 -0300

    inti

---
+++ GestionArchivos.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Parcial_2_TP4_Pascual_Silvestri
+{
+    public partial class GestionArchivos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
